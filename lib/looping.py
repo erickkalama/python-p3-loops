be20a5d3c56094b55774d6e80ea8e749617d30a2
GHOSTFACE <erickkatana4787@gmail.com>
2024-09-15 21:00:52 +0300

    intial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    # For loop to count down from 10 to 1
+    for count in range(10, 0, -1):
+        print(count)
+    
+    # Print the final message
+    print("Happy New Year!")
 
+# Call the function to see the result
+happy_new_year()
 def square_integers(int_list):
-    # code goes here!
+    return[x**2 for x in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Call the function
+fizzbuzz()
+
